69bc15ef4258935aefe067db815777e66ac9798a
Martin Dosch <martin@ideapad>
2018-04-03 23:00:59 +0200

    Send messages as type="chat" so all clients properly show them. Thx @lovetox

--- goxtxt.go
+++ goxtxt.go
@@ -62,7 +62,7 @@ func main() {
 		Address:  configuration.Address,
 		Jid:      configuration.BotJid,
 		Password: configuration.Password,
-		//		PacketLogger: os.Stdout,
+//		PacketLogger: os.Stdout,
 		Insecure: false}
 
 	if _, err := os.Stat(configuration.Twtxtpath); os.IsNotExist(err) {
@@ -91,7 +91,7 @@ func main() {
 				words = strings.Fields(packet.Body)
 				switch strings.ToLower(words[0]) {
 				case "help":
-					reply := xmpp.Message{PacketAttrs: xmpp.PacketAttrs{To: packet.From}, Body: "\"help\": Show this message.\n" +
+					reply := xmpp.Message{PacketAttrs: xmpp.PacketAttrs{To: packet.From, Type: "chat"}, Body: "\"help\": Show this message.\n" +
 						"\"ping\": Bot replies if available.\n" +
 						"\"tl\": Show last " + strconv.Itoa(configuration.TimelineEntries) + " timeline entries.\n" +
 						"\"tv [user]\": Show [user]s timeline.\n" +
@@ -106,26 +106,26 @@ func main() {
 						"\"source\": Shows a link to the sourcecode."}
 					client.Send(reply)
 				case "ping":
-					reply := xmpp.Message{PacketAttrs: xmpp.PacketAttrs{To: packet.From}, Body: "Pong!"}
+					reply := xmpp.Message{PacketAttrs: xmpp.PacketAttrs{To: packet.From, Type: "chat"}, Body: "Pong!"}
 					client.Send(reply)
 				case "source":
-					reply := xmpp.Message{PacketAttrs: xmpp.PacketAttrs{To: packet.From}, Body: "https://github.com/mdosch/goxtxt/"}
+					reply := xmpp.Message{PacketAttrs: xmpp.PacketAttrs{To: packet.From, Type: "chat"}, Body: "https://github.com/mdosch/goxtxt/"}
 					client.Send(reply)
 				case "tw":
 					if len(words) == 1 {
-						reply := xmpp.Message{PacketAttrs: xmpp.PacketAttrs{To: packet.From}, Body: "No Input."}
+						reply := xmpp.Message{PacketAttrs: xmpp.PacketAttrs{To: packet.From, Type: "chat"}, Body: "No Input."}
 						client.Send(reply)
 						break
-					}
+						}
 					if len(packet.Body)-3 > configuration.MaxCharacters {
-						reply := xmpp.Message{PacketAttrs: xmpp.PacketAttrs{To: packet.From}, Body: "Tweet exceeds maximum of " +
+						reply := xmpp.Message{PacketAttrs: xmpp.PacketAttrs{To: packet.From, Type: "chat"}, Body: "Tweet exceeds maximum of " +
 							strconv.Itoa(configuration.MaxCharacters) + " characters."}
 						client.Send(reply)
 						break
 					}
 					_, err := twtxt.Tweet(&configuration.Twtxtpath, words[1:])
 					if err != nil {
-						reply := xmpp.Message{PacketAttrs: xmpp.PacketAttrs{To: packet.From}, Body: "Failed."}
+						reply := xmpp.Message{PacketAttrs: xmpp.PacketAttrs{To: packet.From, Type: "chat"}, Body: "Failed."}
 						client.Send(reply)
 						break
 					}
@@ -133,118 +133,118 @@ func main() {
 				case "tl":
 					out, err := twtxt.Timeline(&configuration.Twtxtpath, &configuration.TimelineEntries)
 					if err != nil {
-						reply := xmpp.Message{PacketAttrs: xmpp.PacketAttrs{To: packet.From}, Body: "Failed." + *out}
+						reply := xmpp.Message{PacketAttrs: xmpp.PacketAttrs{To: packet.From, Type: "chat"}, Body: "Failed." + *out}
 						client.Send(reply)
 						break
 					}
-					reply := xmpp.Message{PacketAttrs: xmpp.PacketAttrs{To: packet.From}, Body: *out}
+					reply := xmpp.Message{PacketAttrs: xmpp.PacketAttrs{To: packet.From, Type: "chat"}, Body: *out}
 					client.Send(reply)
 				case "tv":
 					if len(words) == 1 {
-						reply := xmpp.Message{PacketAttrs: xmpp.PacketAttrs{To: packet.From}, Body: "No Input."}
+						reply := xmpp.Message{PacketAttrs: xmpp.PacketAttrs{To: packet.From, Type: "chat"}, Body: "No Input."}
 						client.Send(reply)
 						break
 					}
 					if len(words) > 2 {
-						reply := xmpp.Message{PacketAttrs: xmpp.PacketAttrs{To: packet.From}, Body: "Timeline view supports only one user."}
+						reply := xmpp.Message{PacketAttrs: xmpp.PacketAttrs{To: packet.From, Type: "chat"}, Body: "Timeline view supports only one user."}
 						client.Send(reply)
 						break
 					}
 					out, err := twtxt.ViewUser(&configuration.Twtxtpath, &configuration.TimelineEntries, &words[1])
 					if err != nil {
-						reply := xmpp.Message{PacketAttrs: xmpp.PacketAttrs{To: packet.From}, Body: "Failed." + *out}
+						reply := xmpp.Message{PacketAttrs: xmpp.PacketAttrs{To: packet.From, Type: "chat"}, Body: "Failed." + *out}
 						client.Send(reply)
 						break
 					}
-					reply := xmpp.Message{PacketAttrs: xmpp.PacketAttrs{To: packet.From}, Body: *out}
+					reply := xmpp.Message{PacketAttrs: xmpp.PacketAttrs{To: packet.From, Type: "chat"}, Body: *out}
 					client.Send(reply)
 				case "tm":
 					if len(words) == 1 {
 						out, err := twtxt.Mentions(&configuration.Twtxtpath, &configuration.Twtxtnick, &configuration.TimelineEntries)
 						if err != nil {
-							reply := xmpp.Message{PacketAttrs: xmpp.PacketAttrs{To: packet.From}, Body: "Failed." + *out}
+							reply := xmpp.Message{PacketAttrs: xmpp.PacketAttrs{To: packet.From, Type: "chat"}, Body: "Failed." + *out}
 							client.Send(reply)
 							break
 						}
-						reply := xmpp.Message{PacketAttrs: xmpp.PacketAttrs{To: packet.From}, Body: *out}
+						reply := xmpp.Message{PacketAttrs: xmpp.PacketAttrs{To: packet.From, Type: "chat"}, Body: *out}
 						client.Send(reply)
 					}
 					if len(words) == 2 {
 						out, err := twtxt.Mentions(&configuration.Twtxtpath, &words[1], &configuration.TimelineEntries)
 						if err != nil {
-							reply := xmpp.Message{PacketAttrs: xmpp.PacketAttrs{To: packet.From}, Body: "Failed." + *out}
+							reply := xmpp.Message{PacketAttrs: xmpp.PacketAttrs{To: packet.From, Type: "chat"}, Body: "Failed." + *out}
 							client.Send(reply)
 							break
 						}
-						reply := xmpp.Message{PacketAttrs: xmpp.PacketAttrs{To: packet.From}, Body: *out}
+						reply := xmpp.Message{PacketAttrs: xmpp.PacketAttrs{To: packet.From, Type: "chat"}, Body: *out}
 						client.Send(reply)
 					}
 					if len(words) > 2 {
-						reply := xmpp.Message{PacketAttrs: xmpp.PacketAttrs{To: packet.From}, Body: "Too many arguments."}
+						reply := xmpp.Message{PacketAttrs: xmpp.PacketAttrs{To: packet.From, Type: "chat"}, Body: "Too many arguments."}
 						client.Send(reply)
 					}
 				case "tt":
 					if len(words) == 1 {
-						reply := xmpp.Message{PacketAttrs: xmpp.PacketAttrs{To: packet.From}, Body: "Missing Input."}
+						reply := xmpp.Message{PacketAttrs: xmpp.PacketAttrs{To: packet.From, Type: "chat"}, Body: "Missing Input."}
 						client.Send(reply)
 					}
 					if len(words) == 2 {
 						out, err := twtxt.Tags(&configuration.Twtxtpath, &words[1], &configuration.TimelineEntries)
 						if err != nil {
-							reply := xmpp.Message{PacketAttrs: xmpp.PacketAttrs{To: packet.From}, Body: "Failed." + *out}
+							reply := xmpp.Message{PacketAttrs: xmpp.PacketAttrs{To: packet.From, Type: "chat"}, Body: "Failed." + *out}
 							client.Send(reply)
 							break
 						}
-						reply := xmpp.Message{PacketAttrs: xmpp.PacketAttrs{To: packet.From}, Body: *out}
+						reply := xmpp.Message{PacketAttrs: xmpp.PacketAttrs{To: packet.From, Type: "chat"}, Body: *out}
 						client.Send(reply)
 					}
 					if len(words) > 2 {
-						reply := xmpp.Message{PacketAttrs: xmpp.PacketAttrs{To: packet.From}, Body: "Too many arguments."}
+						reply := xmpp.Message{PacketAttrs: xmpp.PacketAttrs{To: packet.From, Type: "chat"}, Body: "Too many arguments."}
 						client.Send(reply)
 					}
 				case "tf":
 					if len(words) != 3 {
-						reply := xmpp.Message{PacketAttrs: xmpp.PacketAttrs{To: packet.From}, Body: "Missing Input."}
+						reply := xmpp.Message{PacketAttrs: xmpp.PacketAttrs{To: packet.From, Type: "chat"}, Body: "Missing Input."}
 						client.Send(reply)
 						break
 					}
 					out, err := twtxt.UserManagement(&configuration.Twtxtpath, true, words[1:])
 					if err != nil {
-						reply := xmpp.Message{PacketAttrs: xmpp.PacketAttrs{To: packet.From}, Body: "Failed." + *out}
+						reply := xmpp.Message{PacketAttrs: xmpp.PacketAttrs{To: packet.From, Type: "chat"}, Body: "Failed." + *out}
 						client.Send(reply)
 						break
 					}
-					reply := xmpp.Message{PacketAttrs: xmpp.PacketAttrs{To: packet.From}, Body: *out}
+					reply := xmpp.Message{PacketAttrs: xmpp.PacketAttrs{To: packet.From, Type: "chat"}, Body: *out}
 					client.Send(reply)
 				case "tu":
 					if len(words) != 2 {
-						reply := xmpp.Message{PacketAttrs: xmpp.PacketAttrs{To: packet.From}, Body: "Wrong parameter count."}
+						reply := xmpp.Message{PacketAttrs: xmpp.PacketAttrs{To: packet.From, Type: "chat"}, Body: "Wrong parameter count."}
 						client.Send(reply)
 						break
 					}
 					out, err := twtxt.UserManagement(&configuration.Twtxtpath, false, words[1:])
 					if err != nil {
-						reply := xmpp.Message{PacketAttrs: xmpp.PacketAttrs{To: packet.From}, Body: "Failed." + *out}
+						reply := xmpp.Message{PacketAttrs: xmpp.PacketAttrs{To: packet.From, Type: "chat"}, Body: "Failed." + *out}
 						client.Send(reply)
 						break
 					}
-					reply := xmpp.Message{PacketAttrs: xmpp.PacketAttrs{To: packet.From}, Body: *out}
+					reply := xmpp.Message{PacketAttrs: xmpp.PacketAttrs{To: packet.From, Type: "chat"}, Body: *out}
 					client.Send(reply)
 				case "to":
 					out, err := twtxt.Listfollowing(&configuration.Twtxtpath)
 					if err != nil {
-						reply := xmpp.Message{PacketAttrs: xmpp.PacketAttrs{To: packet.From}, Body: "Failed."}
+						reply := xmpp.Message{PacketAttrs: xmpp.PacketAttrs{To: packet.From, Type: "chat"}, Body: "Failed."}
 						client.Send(reply)
 						break
 					}
-					reply := xmpp.Message{PacketAttrs: xmpp.PacketAttrs{To: packet.From}, Body: *out}
+					reply := xmpp.Message{PacketAttrs: xmpp.PacketAttrs{To: packet.From, Type: "chat"}, Body: *out}
 					client.Send(reply)
 				default:
-					reply := xmpp.Message{PacketAttrs: xmpp.PacketAttrs{To: packet.From}, Body: "Unknown command. Send \"help\"."}
+					reply := xmpp.Message{PacketAttrs: xmpp.PacketAttrs{To: packet.From, Type: "chat"}, Body: "Unknown command. Send \"help\"."}
 					client.Send(reply)
 				}
 			} else {
-				reply := xmpp.Message{PacketAttrs: xmpp.PacketAttrs{To: packet.From}, Body: "You're not allowed to control me."}
+				reply := xmpp.Message{PacketAttrs: xmpp.PacketAttrs{To: packet.From, Type: "chat"}, Body: "You're not allowed to control me."}
 				client.Send(reply)
 				fmt.Fprintf(os.Stdout, "Body = %s - from = %s\n", packet.Body, packet.From)
 			}
